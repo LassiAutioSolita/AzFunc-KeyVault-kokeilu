0139ea2d25b632752248c0a9950ece601d267c5a
Lassi Autio <lassi.autio@solita.fi>
2025-09-03 14:18:09 +0300

    Add AnnaSalaisuusFunction

---
+++ AnnaSalaisuusFunction.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Configuration;
+
+namespace Funktio072;
+
+public class AnnaSalaisuusFunction
+{
+    private readonly IConfiguration _configuration;
+
+    public AnnaSalaisuusFunction(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    // GET /api/AnnaSalaisuus
+    [Function("AnnaSalaisuus")]
+    public HttpResponseData Run(
+        [HttpTrigger(AuthorizationLevel.Function, "get", Route = "AnnaSalaisuus")]
+        HttpRequestData req)
+    {
+        var arvo = _configuration["SALAISUUS"];
+
+        var res = req.CreateResponse(HttpStatusCode.OK);
+        res.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+        res.WriteString(arvo ?? "(SALAISUUS ei ole asetettu)");
+        return res;
+    }
+}
